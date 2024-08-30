ddd82cd2e35a27339b7a1f5137322f778c42f26b
MVictoriaCM <mvittoria7@gmail.com>
2024-08-30 20:57:32 -0300

    correcion

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,7 @@
-window.addEventListener('load', function(){
-    const element = this.document.getElementById("progres");
-    element.style.display ='none';
-})
+window.addEventListener('load', function() {
+    const element = document.getElementById("progres");
+    if (element) {
+      element.style.display = 'none';
+    }
+  });
+  
